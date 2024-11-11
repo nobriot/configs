ee1dc72e6cc12db53734881dc03a29c3a26834a9
Nicolas Woltmann <github@nico-ow.mozmail.com>
2024-11-11 20:01:33 +0100

    Added a few more

--- tmux/tmux-schnecken-bot.sh
+++ tmux/tmux-schnecken-bot.sh
@@ -8,7 +8,7 @@ WINDOW_NAME="schnecken_bot"
 tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? != 0 ]; then
-    tmux new -s $SESSION_NAME\; detach
+  tmux new -s $SESSION_NAME\; detach
 fi
 
 # Check if the window already exists
@@ -16,22 +16,22 @@ tmux list-windows -t $SESSION_NAME | grep -q $WINDOW_NAME
 
 # If the window doesn't exist, create it and set up the panes
 if [ $? != 0 ]; then
-    # Create a new window
-    tmux new-window -t $SESSION_NAME -n $WINDOW_NAME
+  # Create a new window
+  tmux new-window -t $SESSION_NAME -n $WINDOW_NAME
 
-    # Split the window into panes
-    tmux split-window -v -t $SESSION_NAME:$WINDOW_NAME        # Split horizontally
-    tmux split-window -h -t $SESSION_NAME:$WINDOW_NAME.1      # Split vertically
+  # Split the window into panes
+  tmux split-window -v -t $SESSION_NAME:$WINDOW_NAME   # Split horizontally
+  tmux split-window -h -t $SESSION_NAME:$WINDOW_NAME.1 # Split vertically
 
-    # Run commands in specific panes
-    # Schnecken bot
-    tmux select-pane -t $SESSION_NAME:$WINDOW_NAME.1
-    tmux send-keys -t $SESSION_NAME:$WINDOW_NAME.1 'cd /home/nicolas/workspace/schnecken_bot && cargo run --release' C-m # Run cargo in the top left pane
+  # Run commands in specific panes
+  # Schnecken bot
+  tmux select-pane -t $SESSION_NAME:$WINDOW_NAME.1
+  tmux send-keys -t $SESSION_NAME:$WINDOW_NAME.1 'cd /home/nicolas/workspace/schnecken_bot && cargo run --release' C-m # Run cargo in the top left pane
 
-    # htop
-    tmux select-pane -t $SESSION_NAME:$WINDOW_NAME.3
-    tmux resize-pane -t $SESSION_NAME:$WINDOW_NAME.3 -y 30%   # Resize bottom pane to a smaller part
-    tmux send-keys -t $SESSION_NAME:$WINDOW_NAME.3 'htop' C-m # Run htop in the bottom pane
+  # htop
+  tmux select-pane -t $SESSION_NAME:$WINDOW_NAME.3
+  tmux resize-pane -t $SESSION_NAME:$WINDOW_NAME.3 -y 30%   # Resize bottom pane to a smaller part
+  tmux send-keys -t $SESSION_NAME:$WINDOW_NAME.3 'htop' C-m # Run htop in the bottom pane
 fi
 
 # Switch to the window
--- zsh/print-separator.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-x=0; while [ $x -lt $(tput cols) ]; do echo -n '─';let x=$x+1; done;
