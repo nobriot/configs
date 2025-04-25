2415a0a72f83985730f602e4481b597f6cebc06b
Nicolas Woltmann <github@nico-ow.mozmail.com>
2025-04-25 07:14:05 +0200

    Added helix and clean-up nvim config

--- nvim/lua/plugins/ai/copilot.lua
+++ nvim/lua/plugins/ai/copilot.lua
@@ -6,33 +6,3 @@ return {
     { "<leader>cp", "<cmd>Copilot panel<cr>", desc = "Copilot Panel" },
   },
 }
-
--- return {
---   "zbirenbaum/copilot.lua",
---   cmd = "Copilot",
---   build = ":Copilot auth",
---   event = "InsertEnter",
---   opts = {
---     suggestion = {
---       enabled = not vim.g.ai_cmp,
---       auto_trigger = true,
---       keymap = {
---         jump_prev = "<C-p>",
---         jump_next = "<C-n>",
---         accept = "<CR>",
---         refresh = "gr",
---         open = "<C-CR>",
---       },
---       layout = {
---         position = "bottom", -- | top | left | right | horizontal | vertical
---         ratio = 0.4,
---       },
---     },
---     panel = { enabled = false },
---     filetypes = {
---       markdown = true,
---       help = true,
---       ["*"] = true,
---     },
---   },
--- }
